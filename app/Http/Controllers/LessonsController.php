23b5ee9ebd089775fdf0d97288d6decf3b0db1f2
MaratBR <marat01q@gmail.com>
2020-06-07 17:52:26 +0700

    Course teachers editor fix

--- app/Http/Controllers/LessonsController.php
+++ app/Http/Controllers/LessonsController.php
@@ -31,40 +31,4 @@ class LessonsController extends Controller
 
         throw new HttpException(403, "You don't have access to this course");
     }
-
-    public function showAdmin(AdminRequest $request, int $courseId, IEnrollmentService $enrollmentService)
-    {
-        $lesson = $this->lessons->getOverview($courseId);
-
-        return new LessonExDto($lesson);
-    }
-
-    public function store(CreateNewLessonRequest $request)
-    {
-        $this->throwForbiddenIfNotAllowed('create', Lesson::class);
-        return $this->created(
-            $this->lessons->create($request->validated())
-        );
-    }
-
-    public function destroy(DeleteLessonRequest $request)
-    {
-        $lesson = $this->lessons->get($request->lesson);
-        $this->throwForbiddenIfNotAllowed('delete', $lesson);
-        return $this->deleteShortcut(
-            $this->lessons->delete(
-                $lesson,
-                $request->isForce()
-            )
-        );
-    }
-
-    public function update(UpdateLessonRequest $request)
-    {
-        $lesson = $this->lessons->get($request->lesson);
-        $this->throwForbiddenIfNotAllowed('update   ', $lesson);
-        $this->updateShortcut(
-            $lesson->update($request->validated())
-        );
-    }
 }
